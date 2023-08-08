f58839b1d0a54373993cd327f868c8f3ffd1743c
Max <mrbroo123@gmail.com>
2023-08-08 12:49:46 +0300

    переменная symbol

--- Program.cs
+++ Program.cs
@@ -1,3 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-git remote add origin https://github.com/flasptap/KR.git
+string[] symbol(string[] array)
