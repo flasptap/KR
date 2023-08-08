13adb06fc816f9dde39e7184323f48e62a73f69e
Max <mrbroo123@gmail.com>
2023-08-08 12:51:00 +0300

    написание программы , вывод данных

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
 string[] symbol(string[] array)
+{
+    string[] Newsymbol = new string[11];
+
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Newsymbol[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref Newsymbol, count);
+    return Newsymbol;
+}
+void Print(string[] array)
+{
+    Console.Write("[" + string.Join(", ", array) + "]");
+    Console.WriteLine();
+}
+
+string[] NewArray = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
+
+Print(symbol(NewArray));
